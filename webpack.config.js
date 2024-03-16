e25aa7ee219398498957fca68ca2065740cbeee6
Raramuri <raramuri@Raramuris-MacBook-Pro.local>
2024-03-16 14:39:26 +0530

    sda

--- webpack.config.js
+++ webpack.config.js
@@ -44,7 +44,9 @@ module.exports = (_, argv) => ({
       name: "demo",
       filename: "remoteEntry.js",
       remotes: {},
-      exposes: {},
+      exposes: {
+        "./App": "./src/App.jsx",
+      },
       shared: {
         ...deps,
         react: {
